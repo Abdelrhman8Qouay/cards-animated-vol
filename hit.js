1d3a469954a8ea653d6caa0bbc5ea0d743ea9ba7
abdelrhman ashraf <mido59478@gmail.com>
2023-11-20 19:03:14 +0200

    update animated done

--- hit.js
+++ hit.js
@@ -1,28 +1,37 @@
 
-const card_boxes = document.querySelectorAll('.card_box');
+const card_boxes = document.querySelectorAll('.card_box'),
+modal = document.querySelector('.modal'),
+startBtn = document.getElementById('start');
+
+
 var overAud = new Audio("./assets/audio/over.mp3");
 var outAud = new Audio("./assets/audio/out.mp3");
 
-
 let hovered =  false;
-card_boxes.forEach((box)=> {
-    box.addEventListener('pointerover', ()=> {
-        if (!hovered) playAudio(overAud);
-        hovered = true;
-    });
-    box.addEventListener('pointerleave', ()=> {
-        hovered = false;
-        playAudio(outAud);
-    });
+
+startBtn.onclick = ()=> {
+    modal.classList.add('hide');
+    card_boxes.forEach((box)=> {
+        box.addEventListener('pointerover', ()=> {
+            if (!hovered) playAudio(overAud);
+            hovered = true;
+        });
+        box.addEventListener('pointerleave', ()=> {
+            hovered = false;
+            playAudio(outAud);
+        });
 
 
-    for (i = 1; i <= 100; i++) {
-        box.children[2].innerHTML += `<div class="circle-container">
-        <div class="circle"></div>
-    </div>`;
-    }
-});
+        for (i = 1; i <= 100; i++) {
+            box.children[2].innerHTML += `<div class="circle-container">
+            <div class="circle"></div>
+        </div>`;
+        }
+    });
+}
+
 
 function playAudio(aud) {
+    aud.volume = '0.4';
     aud.play();
 }
